9dc28fd0d753f202c6fb7ef3ecf122e7390f1cd1
Amir Hamja Ratul <amirratul2005@gmail.com>
2026-08-31 00:14:38 +0600

    Update app.py

--- app.py
+++ app.py
@@ -22,8 +22,9 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-COURSES = {
-    "PYQ": "Previous Year Questions",
+COURSES =
+{
+
     "ESE 2101": "Hydrology and Hydrogeology",
     "ESE 2103": "Oceanography and Limnology",
     "ESE 2105": "Ecology",
@@ -35,6 +36,8 @@ COURSES = {
     "ESE 2111": "Soil Mechanics",
     "ESE 2108": "Engineering Drawing Lab",
     "ESE 2113": "Statistics for Environment"
+     "PYQ": "Previous Year Questions",
+     "MEQ": "Mid Exam Questions",
 }
 
 course_options = [f"{code} - {title}" for code, title in COURSES.items()]
